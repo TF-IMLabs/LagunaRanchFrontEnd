e3a1948ad2c7167e525c0069269e60a774f3304f
Tomas Ferrari <tomas_ferrari@outlook.com>
2025-03-22 21:59:40 -0300

    ultimo

--- src/components/theme.js
+++ src/components/theme.js
@@ -2,34 +2,34 @@ import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
     typography: {
-        fontFamily: '"Verdana", "Roboto", "Georgia", "Arial", sans-serif', 
+        fontFamily: '"Roboto", "Roboto", "Georgia", "Arial", sans-serif', 
         h1: {
-            fontFamily: '"Verdana", "Roboto", "Georgia", "Arial", sans-serif',
+            fontFamily: '"Roboto", "Roboto", "Georgia", "Arial", sans-serif',
         },
         h2: {
-            fontFamily: '"Verdana", "Roboto", "Georgia", "Arial", sans-serif',
+            fontFamily: '"Roboto", "Roboto", "Georgia", "Arial", sans-serif',
         },
         h4: {
-            fontFamily: '"Verdana", "Roboto", "Georgia", "Arial", sans-serif',
+            fontFamily: '"Roboto", "Roboto", "Georgia", "Arial", sans-serif',
             color: '#DD98AD',
         },
         h5: {
-            fontFamily: '"Verdana", "Roboto", "Georgia", "Arial", sans-serif',
+            fontFamily: '"Roboto", "Roboto", "Georgia", "Arial", sans-serif',
             color: '#DD98AD',
             fontSize: '1.4rem',
         },
         h5b: {
-            fontFamily: '"Verdana", "Roboto", "Georgia", "Arial", sans-serif',
+            fontFamily: '"Roboto", "Roboto", "Georgia", "Arial", sans-serif',
             color: 'black',
             fontSize: '1.4rem',
         },
         h6: {
-            fontFamily: '"Verdana", "Roboto", "Georgia", "Arial", sans-serif',
+            fontFamily: '"Roboto", "Roboto", "Georgia", "Arial", sans-serif',
             color: 'white',
             fontSize: '1.2rem',
         },
         h6b: {
-            fontFamily: '"Verdana", "Roboto", "Georgia", "Arial", sans-serif',
+            fontFamily: '"Roboto", "Roboto", "Georgia", "Arial", sans-serif',
             color: 'black',
             fontSize: '1.2rem',
         },
